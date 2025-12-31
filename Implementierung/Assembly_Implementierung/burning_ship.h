908b667dc6ec1a444e31d94436c3a51fec90f59b
Joel <jcjoelchiu@gmail.com>
2025-12-31 12:11:12 +0100

    Added header files and Makefile for the assembly implementation

---
+++ Implementierung/Assembly_Implementierung/burning_ship.h
@@ -0,0 +1,16 @@
+#ifndef BURNING_SHIP_H
+#define BURNING_SHIP_H
+
+#include <complex.h>
+#include <stddef.h>
+
+void burning_ship(
+  float complex start,
+  size_t width,
+  size_t height,
+  float res,
+  unsigned n, 
+  unsigned char* img
+);
+
+#endif 
---
+++ Implementierung/Assembly_Implementierung/main.c
@@ -0,0 +1,9 @@
+#include "bmp.h"
+#include "burning_ship.h"
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
