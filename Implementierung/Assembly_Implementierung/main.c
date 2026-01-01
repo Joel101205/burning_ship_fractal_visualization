e96e0fe53a777ce76dd1cde20494e2ef670d2547
Joel <jcjoelchiu@gmail.com>
2026-01-01 22:47:29 +0100

    wrote assembly implementation, currently not working properly lol

--- Implementierung/Assembly_Implementierung/main.c
+++ Implementierung/Assembly_Implementierung/main.c
@@ -5,5 +5,21 @@
 
 int main(int argc, char *argv[])
 {
+  size_t width = 1600;
+  size_t height = 1000;
+  float complex start = -1.8 + 0.0 * I;
+  float res = 0.0001;
+  unsigned n = 1000;
+
+  unsigned char *img = malloc(width * height * 3);
+
+  if (img == NULL) return 1;
+
+  burning_ship(start, width, height, res, n, img);
+
+  Pixel *pixels = (Pixel *) img;
+
+  write_pixel(pixels, width, height);
+
   return EXIT_SUCCESS;
 }
